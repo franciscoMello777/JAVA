2e2160b2fb85c068ae29757c5b7d74bb338b4fa0
franciscoMello777 <162368791+franciscoMello777@users.noreply.github.com>
2024-10-30 14:52:22 -0300

    Add files via upload

---
+++ personagem/Guerreiro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personagem;
+
+/**
+ *
+ * @author FRANCISCOPESDEMELLO
+ */
+public class Guerreiro extends Personagem{
+    
+    public Guerreiro(String nome) {
+        super(nome, 100, 15);
+}
+    
+    
+}
