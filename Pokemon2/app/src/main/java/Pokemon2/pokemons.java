3a496ac6afacf70c6be39d72c94e5e90f6629339
franciscoMello777 <162368791+franciscoMello777@users.noreply.github.com>
2024-12-16 14:33:51 -0300

    Add files via upload

---
+++ Pokemon2/app/src/main/java/Pokemon2/pokemons.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pokemon2;
+
+/**
+ *
+ * @author FRANCISCOPESDEMELLO
+ */
+public class pokemons {
+    
+}
---
+++ exe.aula3/src/exe/aula3/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exe.aula3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author FRANCISCOPESDEMELLO
+ */
+public class Main {
+    public static void main(String[] args) {
+        RPSGame game = new RPSGame(); // Inicializa o jogo
+        game.start(); // Inicia o jogo
+    }
+}
