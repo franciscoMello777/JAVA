b9a5c793d0a7600fde5c25427eb54126b59fb6b8
franciscoMello777 <162368791+franciscoMello777@users.noreply.github.com>
2024-12-16 14:30:24 -0300

    Add files via upload

---
+++ Conexao/app/src/main/java/Conexao/InserirUsuario.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author FRANCISCOPESDEMELLO
+ */
+public class InserirUsuario {
+    public static void inserirUsuario(Connection conexao, String nome, String email) {
+        String sql = "INSERT INTO usuarios (nome, email) VALUES (?, ?)";
+        
+        try (PreparedStatement pstmt = conexao.prepareStatement(sql)) {
+            pstmt.setString(1, nome);
+            pstmt.setString(2, email);
+            pstmt.executeUpdate();
+            System.out.println("Usuario insirido com sucesso!");
+        } catch (Exception e) {
+            System.out.println("Erro ao inserir o usuario!" + e.getMessage());
+        }
+    }
+}
